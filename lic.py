c3aadf83c5fce36cc99c345c2fe80e2ff282a6a5
Trevor <hill341@purdue.edu>
2020-04-27 19:57:02 -0400

    Added initial testing functionality

--- lic.py
+++ lic.py
@@ -1,24 +1,172 @@
 #!/usr/bin/python
 
+# TODO: Server IP and Port is hardcoded to localhost. Need to make them dynamic based on parameter input (and perhaps even make it auto-find servers using UDP broadcast?)
+# TODO: Handle Rate argument input (i.e. do we want to specify '1000' or '1kpbs', '1000000000' or '1gbps', etc.)
+# TODO: Sleep Timer and Packet Size need to be amended (MacOS sends 1 packet per byte! ouch)
+# TODO: Result table needs to be pretty printed a little better with zero padding
+
 import argparse
+import json
+import random
 import socket
 import sys
 import time
+import traceback
 
-if __name__ == "__main__":
+def main():
 
     # Parse required arguments
     parser = argparse.ArgumentParser(description='LAN Integrity Tester Client')
-    parser.add_argument('Rounds', type=int, help='The number of tranmission rounds to be performed (number of divisions of the transmission rate)')
-    parser.add_argument('Rate', type=int, help='The maximum rate of data transfer to be incrementally tested up to')
-    parser.add_argument('-a', dest='address', type=int, nargs='?', help='The IP address of the desired server')
+    parser.add_argument('rounds', type=int, help='The number of tranmission rounds to be performed (number of divisions of the transmission rate) (max 25)')
+    parser.add_argument('rate', type=int, help='The maximum rate of data transfer to be incrementally tested up to (max 1gbps)')
+    parser.add_argument('-a', dest='address', type=str, nargs='?', help='The IP address of the desired server')
     parser.add_argument('-p', dest='port', type=int, nargs='?', help='The port number of the desired server')
     args = parser.parse_args()
     
-    # s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # message = b'Hello World'
-    # addr = ("127.0.0.1", PORT)
+    # Check argument validity
+    if args.rounds < 1 or args.rounds > 25:
+        print("Error: Argument 'rounds' must be in the range 0 < x <= 25")
+        exit(1)
 
-    # for i in range(10):
-    #     s.sendto(message, addr)
-    #     time.sleep(1)
+    # Determine round and datarate information
+    max_rounds = args.rounds if args.rounds else 10
+    max_rate = args.rate if args.rate else 1000000000
+    increment = max_rate / max_rounds
+
+    # Establish a connection to the remote server
+    print("Establishing a connection to the test server...")
+    try:
+        tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp_socket.connect(('localhost', 62994))
+    except:
+        print("Failed to establish a connection to the server. Aborting...")
+        traceback.print_exc()
+        exit(1)
+    print("Successfully established a connection to the test server.")
+
+    print("Setting up testing environment...")
+    try:
+        # Create synchronization request and send to remote server as JSON
+        request = {
+            'status': 'synchronize',
+        }
+        tcp_socket.send(json.dumps(request).encode('utf-8') + b'\n')
+
+        # Listen for server syn-ack response
+        data = bytearray()
+        byte = tcp_socket.recv(1)
+        while byte != '':
+            if b'\n' == byte:
+                break
+            data += byte
+            byte = tcp_socket.recv(1)
+
+        # Extract server's UDP port number from server response
+        response = json.loads(data.decode('utf-8'))
+
+        if 'status' not in response or response['status'] != 'synchronize-ack': 
+            raise Exception('Server did not syn-ack')
+            return
+
+        udp_port = response['udp_port']
+
+        # Create UDP connection to server at specified port
+        udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    except:
+        print("Failed to establish testing environment. Aborting...")
+        traceback.print_exc()
+        exit(1)
+    print("Successfully established testing environmnet.")
+
+    # Establish storage for result data
+    results = []
+
+    print("Beginning testing procedure...")
+    # Iterate over rounds
+    current_round = 1
+    while current_round <= max_rounds:
+        print(f"Running round {current_round} at rate {current_round * increment}")
+        # Compute amount of data to be sent given a 5 second duration
+        current_rate = current_round * increment
+        byte_count = int(current_rate * 5 / 8)
+        # sleep_time = 5 / byte_count / 8
+        sleep_time = 0
+        print(f"Current Rate: {current_rate}, byte_count: {byte_count}, sleep_time: {sleep_time}")
+
+        # Send server current round configuration JSON
+        config = {
+            'status': 'test_in_progress',
+            'round': current_round,
+            'rate': current_rate,
+            'byte_count': byte_count
+        }
+
+        tcp_socket.send(json.dumps(config).encode('utf-8') + b'\n')
+
+        # Await server status response
+        data = bytearray()
+        byte = tcp_socket.recv(1)
+        while byte != '':
+            if byte == b'\n':
+                break
+            data += byte
+            byte = tcp_socket.recv(1)
+    
+        if (json.loads(data.decode('utf-8'))['status'] != 'ready'):
+            print("PANIC")
+            exit(1)
+
+        # Send UDP one byte at a time, with payload counting up from 0
+        payload = bytearray(1)
+        payload[0] = 0
+        for x in range(byte_count):
+            # print("payload:" + str(payload))
+            udp_socket.sendto(payload, ('localhost', udp_port))
+            payload[0] = (payload[0] + 1) % 255
+            # time.sleep(sleep_time)
+
+        # Signal server that round is complete
+        tcp_socket.send(json.dumps({ 'status': 'round_complete'}).encode('utf-8') + b'\n')
+        print(f"Round {current_round} complete")
+
+        # Wait for server ready confirmation before proceeding to next round
+        data = bytearray()
+        byte = tcp_socket.recv(1)
+        while byte != '':
+            if b'\n' == byte:
+                break
+            data += byte
+            byte = tcp_socket.recv(1)
+
+        # Prepare for next testing round
+        current_round = current_round + 1
+
+    # Testing Complete.
+    # Notify server that test is complete
+    message = {
+        'status': 'test_complete',
+    }
+
+    tcp_socket.send(json.dumps(message).encode('utf-8') + b'\n')
+
+    # Retrieve results from server
+    data = bytearray()
+    byte = tcp_socket.recv(1)
+    while byte != '':
+        if b'\n' == byte:
+            break
+        data += byte
+        byte = tcp_socket.recv(1)
+
+    results = json.loads(data.decode('utf-8'))
+    print(results)
+    print("Testing Complete")
+    print("Results:")
+    print("--------------------------------------------------")
+    print("Round    Rate       Lost      Rating")
+    for result in results:
+        print(f"{result['round']:02d}       {result['rate']}      {result['lost']}%     {result['rating']}")
+    print("--------------------------------------------------")
+
+if __name__ == "__main__":
+    main()
--- lis.py
+++ lis.py
@@ -1,30 +1,164 @@
 #!/usr/bin/python
 
 import argparse
+import json
 import socket
 import sys
 import time
+import threading
 
-if __name__ == "__main__":
+def main():
 
     # Parse required arguments
     parser = argparse.ArgumentParser(description='LAN Integrity Tester Client')
     parser.add_argument('-p', dest='port', type=int, nargs='?', help='The desired TCP port for the server to bind to')
     args = parser.parse_args()
 
+    # Check argument validity
+    if args.port:
+        if args.port < 0 or args.port > 65535:
+            print("Error: Argument 'port' must be in the range 0 < x <= 65535")
+            exit(1)
+        if args.port < 1024:
+            print("Warning: Argument 'port' is a well-defined port. This may require superuser permissions")
+        elif args.port < 49151:
+            print("Warning: Argument 'port' is a registered port. Port collision is possible")
+    
     # Extract parsed arguments
     if (args.port):
         tcp_port = args.port
     else:
-        tcp_port = 69420
+        tcp_port = 62994
 
-    # s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # s.bind(('',PORT))
-    # lastMsgTime = False
-    # while True:
-    #     data, address = s.recvfrom(1024)
-    #     print(data)
-    #     if lastMsgTime:
-    #         elapsed = time.time() - lastMsgTime
-    #         print(elapsed)
-    #     lastMsgTime = time.time()
+    # Establish server at specified port number and await a connection
+    print(f"Establishing listening server on port {tcp_port}...")
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.bind(('localhost', tcp_port))
+    tcp_socket.listen(1)
+
+    # Serve indefinitely
+    while True:
+        tcp_conn, tcp_addr = tcp_socket.accept()
+
+        # Service new connection
+        print(f"Connection established by address {tcp_addr}")
+        TCP_Connection_Handler(tcp_conn)
+
+# Handles TCP connections for handling active tests. 
+# Is NOT intended to be threaded
+def TCP_Connection_Handler(tcp_conn):
+    # Await and decode connection synchronize request in JSON
+    data = bytearray()
+    byte = tcp_conn.recv(1)
+    while byte != '':
+        if b'\n' == byte:
+            break
+        data += byte
+        byte = tcp_conn.recv(1)
+
+    message = json.loads(data.decode('utf-8'))
+
+    if 'status' not in message or message['status'] != 'synchronize': 
+        print("Connection did not properly synchronize")
+        return
+
+    # Bind UDP socket to a OS-specified port number
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind(('localhost', 0))
+
+    # Send OS-allocated UDP port number to client as JSON
+    response = {
+        'status': 'synchronize-ack',
+        'udp_port': udp_socket.getsockname()[1]
+    }
+    tcp_conn.send(json.dumps(response).encode('utf-8') + b'\n')
+
+    # Global round result storage
+    results = []
+
+    # Handle each transmission round until client terminates test
+    while True:
+        # Await and decode round configuration JSON
+        data = bytearray()
+        byte = tcp_conn.recv(1)
+        while byte != '':
+            if b'\n' == byte:
+                break
+            data += byte
+            byte = tcp_conn.recv(1)
+
+        round_config = json.loads(data.decode('utf-8'))
+
+        # If testing is complete, compute results, return to sender, and terminate connection
+        if (round_config['status'] == 'test_complete'):
+            print("Test complete... Returning")
+            tcp_conn.send(json.dumps(results).encode('utf-8') + b'\n')
+            tcp_conn.close()
+            return
+
+        # Testing has proceeded to the next round. Spawn handler and signal ready
+        stop_signal = False
+        payload_map = dict()
+        listener_thread = threading.Thread(target=UDP_Listener, args=(udp_socket, payload_map, lambda: stop_signal,))
+        listener_thread.start()
+
+        tcp_conn.send(json.dumps({'status': 'ready'}).encode('utf-8') + b'\n')
+
+        # Await round completion JSON from client
+        data = bytearray()
+        byte = tcp_conn.recv(1)
+        while byte != '':
+            if b'\n' == byte:
+                break
+            data += byte
+            byte = tcp_conn.recv(1)
+
+        # Signal UDP listening thread to terminate and then await it
+        stop_signal = True
+        listener_thread.join()
+
+        # Compute round results
+        bytes_received = 0
+        # for x in range(255):
+            # bytes_received = bytes_received + payload_map[x]
+        for key in payload_map:
+            bytes_received = bytes_received + payload_map[key]
+        
+        lost_percent = (round_config['byte_count'] - bytes_received) / round_config['byte_count'] * 100
+        rating = 'pass'
+        
+        if lost_percent > 1:
+            rating = 'acceptable'
+        if lost_percent > 7:
+            rating = 'fail'
+        
+        results.append({
+            'round': round_config['round'],
+            'rate': round_config['rate'],
+            'lost': lost_percent,
+            'rating': rating
+        })
+
+        print(results)
+        # Signal client that server is ready for the next round
+        tcp_conn.send(json.dumps({'status': 'ready'}).encode('utf-8') + b'\n')
+
+# Handles UDP listening on a separate thread so that the TCP connection can be monitored by main thread for status updates
+# Param: udp_socket: The udp socket to be monitored
+# Param: payload_map: The key-value dictionary, used such that payload_map[payload]++ to count payload instances
+# Param: signal: A boolean that signifies whether the thread should terminate after a period of no new packets
+def UDP_Listener(udp_socket, payload_map, signal):
+    while True:
+        udp_socket.settimeout(1)
+        try:
+            udp_msg, udp_addr = udp_socket.recvfrom(1)
+            if int.from_bytes(udp_msg, 'little') in payload_map:
+                payload_map[int.from_bytes(udp_msg, 'little')] = payload_map[int.from_bytes(udp_msg, 'little')] + 1
+            else:
+                payload_map[int.from_bytes(udp_msg, 'little')] = 1
+        except socket.timeout:
+            if signal():
+                return
+
+if __name__ == "__main__":
+    main()
