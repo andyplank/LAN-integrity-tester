1d0a24f0b8bb2ad368b1e49a1d6beb47183f1b13
Trevor <hill341@purdue.edu>
2020-04-26 23:21:53 -0400

    Added basic argument parsing

--- lic.py
+++ lic.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python
 
+import argparse
 import socket
 import sys
 import time
 
-PORT = 8080
-
 if __name__ == "__main__":
 
-    ## ARG CHECKING ##
-    # if(len(sys.argv)!=2):
-    #     print('Invalid args.\nTry ./test.py <PORT>')
-    #     exit(1)
-    # else:
-    #     try:
-    #         PORT = int(sys.argv[1])
-    #     except ValueError:
-    #         print('Invalid args.\nPort must be a number')
-    #         exit(1)
+    # Parse required arguments
+    parser = argparse.ArgumentParser(description='LAN Integrity Tester Client')
+    parser.add_argument('Rounds', type=int, help='The number of tranmission rounds to be performed (number of divisions of the transmission rate)')
+    parser.add_argument('Rate', type=int, help='The maximum rate of data transfer to be incrementally tested up to')
+    parser.add_argument('-a', dest='address', type=int, nargs='?', help='The IP address of the desired server')
+    parser.add_argument('-p', dest='port', type=int, nargs='?', help='The port number of the desired server')
+    args = parser.parse_args()
+    
+    # s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # message = b'Hello World'
+    # addr = ("127.0.0.1", PORT)
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    message = b'Hello World'
-    addr = ("127.0.0.1", PORT)
-
-    for i in range(10):
-        s.sendto(message, addr)
-        time.sleep(1)
+    # for i in range(10):
+    #     s.sendto(message, addr)
+    #     time.sleep(1)
--- lis.py
+++ lis.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python
 
+import argparse
 import socket
 import sys
 import time
 
-PORT = 8080
-
 if __name__ == "__main__":
 
-    ## ARG CHECKING ##
-    # if(len(sys.argv)!=2):
-    #     print('Invalid args.\nTry ./server.py <PORT>')
-    #     exit(1)
-    # else:
-    #     try:
-    #         PORT = int(sys.argv[1])
-    #     except ValueError:
-    #         print('Invalid args.\nPort must be a number')
-    #         exit(1)
+    # Parse required arguments
+    parser = argparse.ArgumentParser(description='LAN Integrity Tester Client')
+    parser.add_argument('-p', dest='port', type=int, nargs='?', help='The desired TCP port for the server to bind to')
+    args = parser.parse_args()
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(('',PORT))
-    lastMsgTime = False
-    while True:
-        data, address = s.recvfrom(1024)
-        print(data)
-        if lastMsgTime:
-            elapsed = time.time() - lastMsgTime
-            print(elapsed)
-        lastMsgTime = time.time()
+    # Extract parsed arguments
+    if (args.port):
+        tcp_port = args.port
+    else:
+        tcp_port = 69420
+
+    # s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # s.bind(('',PORT))
+    # lastMsgTime = False
+    # while True:
+    #     data, address = s.recvfrom(1024)
+    #     print(data)
+    #     if lastMsgTime:
+    #         elapsed = time.time() - lastMsgTime
+    #         print(elapsed)
+    #     lastMsgTime = time.time()
